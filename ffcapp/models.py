6d88e2421c3a0a1c854c54b7288ae9f7c3d994ae
whduddhks <jyy1551@naver.com>
2020-01-17 15:54:20 +0900

    카페 등록 모델 수정

--- ffcapp/models.py
+++ ffcapp/models.py
@@ -6,12 +6,12 @@ from django.core.validators import MinValueValidator,MaxValueValidator
 class Cafe(models.Model):
     # id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, null=True)
-    address = models.TextField(null=True)
+    address = models.CharField(max_length=150, null=True)
     phonenumber = models.CharField(max_length=20,null=True)
     start_time = models.TimeField('start_time',null=True)
     end_time = models.TimeField('end_time',null=True)
     holiday = models.CharField(max_length=27,null=True)
-    content = models.TextField(null=True) #태그
+    content = models.CharField(max_length=1500, null=True) #태그
     menu = models.ImageField(upload_to='images/',null=True, blank = True)
     choice = (
         ('yes', '있음'),
